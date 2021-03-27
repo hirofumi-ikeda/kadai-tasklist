abe1ef9abbb937b0666744fc5b66af4e91ac31ac
hirofumi ikeda <h.ikeda@bir-sys.com>
2021-03-27 11:19:14 +0900

    Add validation

---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,30 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Tasks;
+
+public class MessageValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Tasks m) {
+        List<String> errors = new ArrayList<String>();
+
+        String content_error = validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // タスク内容の入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "タスク内容を入力してください。";
+        }
+
+        return "";
+    }
+
+}
